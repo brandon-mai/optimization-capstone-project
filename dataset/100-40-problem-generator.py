750531c6dbdc9ba14487c1981097d6cee05a1b73
brandon-mai <anhbinhbao0@gmail.com>
2024-01-07 22:55:15 +0700

    more 100 40 problems

--- dataset/100-40-problem-generator.py
+++ dataset/100-40-problem-generator.py
@@ -5,7 +5,7 @@ def main():
     t = time.localtime()
     filename = (f"input100_{t.tm_mday:0>2d}{t.tm_mon:0>2d}{str(t.tm_year % 100).zfill(2)}"
                 f"_{t.tm_hour:0>2d}{t.tm_min:0>2d}{t.tm_sec:0>2d}")
-    f = open(f"dataset/{filename}.txt", "w")
+    f = open(f"{filename}.txt", "w")
     f.write("100 40\n")
     for i in range(201):
         for j in range(201):
